553bcbc0009e9a86ef0a21f3e4440dac847d6047
M-web186 <maureenwyse667@gmail.com>
2025-06-03 09:39:43 +0300

    moh.cpp
    factorial of a number

---
+++ moh.cpp
@@ -0,0 +1,23 @@
+//factorial of a number
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int factorial(int);
+int fact,value;
+cout<<"enter any number: ";
+cin>>value;
+fact = factorial(value);
+cout<<" factorial of a number is: "<<fact<<endl;
+return 0;
+}
+int factorial(int n)
+{
+if(n<0)
+    return (-1);)  /*wrong value*/
+if(n==0)  /*terminating condition*/
+    return(1);
+else
+return(n* factorial(n-1));
+}
